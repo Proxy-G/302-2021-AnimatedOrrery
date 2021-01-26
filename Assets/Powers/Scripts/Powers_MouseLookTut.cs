267525d4fec35195769d967aa2d0f71cb1247000
Aaron Powers <aahproxy@Gmail.com>
2021-01-26 02:33:01 -0500

    Minor Bug Fixes
    -fixed problem with button select audio still playing before buttons are activated

--- Assets/Powers/Scripts/Powers_MouseLookTut.cs
+++ Assets/Powers/Scripts/Powers_MouseLookTut.cs
@@ -5,12 +5,13 @@ using UnityEngine.UI;
 
 public class Powers_MouseLookTut : MonoBehaviour
 {
-    public CanvasGroup normalUI;
+    public GameObject normalUI;
     
     private bool tutCompleted = false;
     private Image tutImage;
     private Vector2 camDirection = Vector2.zero;
 
+    private CanvasGroup normalUIcanvas;
     private float tutAlpha;
     private float normAlpha;
 
@@ -21,8 +22,10 @@ public class Powers_MouseLookTut : MonoBehaviour
         tutAlpha = tutImage.color.a;
 
         //Disable the normal UI for the camera rotate tutorial
-        normalUI.alpha = 0;
-        normalUI.interactable = false;
+        normalUIcanvas = normalUI.GetComponent<CanvasGroup>();
+        normalUIcanvas.alpha = 0;
+        normalUIcanvas.interactable = false;
+        normalUI.SetActive(false);
     }
     void Update()
     {
@@ -54,23 +57,26 @@ public class Powers_MouseLookTut : MonoBehaviour
             }
                 
             //If image color alpha is 0 and canvas group alpha is not 1, start work on norm UI alpha.
-            else if (tutImage.color.a == 0 && normalUI.alpha != 1)
+            else if (tutImage.color.a == 0 && normalUIcanvas.alpha != 1)
             {
+                //activate normal ui holder. this was to prevent sfx from playing before the transition starts.
+                normalUI.SetActive(true);
+
                 //Set tut alpha to current image alpha
-                normAlpha = normalUI.alpha;
+                normAlpha = normalUIcanvas.alpha;
 
                 //Lerp alpha down to 0 if not close, otherwise set to 0
-                if (normAlpha < 0.99f) normAlpha = Powers_AnimMath.Lerp(normalUI.alpha, 1, 0.05f, false);
+                if (normAlpha < 0.99f) normAlpha = Powers_AnimMath.Lerp(normalUIcanvas.alpha, 1, 0.05f, false);
                 else normAlpha = 1;
 
                 //Set image color alpha to temp alpha
-                normalUI.alpha = normAlpha;
+                normalUIcanvas.alpha = normAlpha;
             }
 
             //If normal UI alpha is now 1, the tut is complete. Set canvas group interactable to true and destroy this.
-            else if (tutImage.color.a == 0 && normalUI.alpha == 1)
+            else if (tutImage.color.a == 0 && normalUIcanvas.alpha == 1)
             {
-                normalUI.interactable = true;
+                normalUIcanvas.interactable = true;
                 Destroy(this);
             }
         }
